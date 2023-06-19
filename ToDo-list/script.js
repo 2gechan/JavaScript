548faa39c37a36eb4b008d759496b0e0f11ffcd5
2gechan <7532602@gmail.com>
2023-06-19 20:53:12 +0900

    localStorage

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,7 +1,21 @@
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
-const createTodo = function () {
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+console.log(savedTodoList);
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
+
+const createTodo = function (storageData) {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
   // createElement : 태그를 생성해낸다.
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
@@ -10,6 +24,7 @@ const createTodo = function () {
   newBtn.addEventListener("click", () => {
     // 첫 클릭시 클래스 complete를 생성 다음 클릭 시 삭제
     newLi.classList.toggle("complete");
+    saveItemsFn();
   });
 
   // dblclick -> 더블클릭
@@ -17,7 +32,7 @@ const createTodo = function () {
     newLi.remove();
   });
   // 생성한 span태그에 value 저장
-  newSpan.textContent = todoInput.value;
+  newSpan.textContent = todoContents;
   newLi.appendChild(newBtn);
   // 생성한 li태그에 위 코드에서 생성한 span 태그 넣기
   newLi.appendChild(newSpan);
@@ -35,8 +50,8 @@ const keyCodeCheck = () => {
 
 const deleteAll = () => {
   const liList = document.querySelectorAll("li");
-  for (let li of liList) {
-    li.remove();
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
   }
 };
 
@@ -50,5 +65,6 @@ const saveItemsFn = () => {
     };
     saveItems.push(todoObj);
   }
-  console.log(saveItems);
+
+  localStorage.setItem("saved-items", JSON.stringify(saveItems));
 };
